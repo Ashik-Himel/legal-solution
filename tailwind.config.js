68006fba9029fc7e29c886b65fd27be2c52364f1
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-07-25 11:46:13 +0600

    nav and hero banner done

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,16 @@ module.exports = {
         'card-bg': '#F3F3F3',
       }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      }
+    }
   },
   plugins: [require('daisyui')],
   daisyui: {
